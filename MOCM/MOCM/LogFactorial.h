318896021e239ba156d81b7ec99fc45dcdf320f6
MoriartySsss <m3si@uwaterloo.ca>
2021-07-21 02:29:18 +0800

    Lasest Version!!!

---
+++ MOCM/MOCM/LogFactorial.h
@@ -0,0 +1,13 @@
+//
+//  LogFactorial.h
+//  MOCM
+//
+//  Created by MuYao Si on 2021-07-20.
+//
+
+#ifndef LogFactorial_h
+#define LogFactorial_h
+
+double LogFactorial(int n);
+
+#endif /* LogFactorial_h */
